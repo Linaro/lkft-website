59cd0a4b4c6f7899596bd0f72e7d43acfb876659
Philip Colmer <philip.colmer@linaro.org>
2019-07-01 14:32:24 +0100

    Drop capabilities when calling Docker

--- build-site.sh
+++ build-site.sh
@@ -7,7 +7,17 @@ fi
 if [ -z "$JEKYLL_ENV" ]; then
 	export JEKYLL_ENV=staging
 fi
-
-docker run --rm -it -p 4000:4000 -e JEKYLL_ACTION -e JEKYLL_CONFIG -e JEKYLL_ENV \
-        -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -u "$(id -u)":"$(id -g)" \
-        -v "$(pwd)":/srv/source linaroits/jekyllsitebuild:"$JEKYLLSITEBUILD" build-site.sh
+docker run \
+  --cap-drop ALL \
+  --rm \
+  -it \
+  -p 4000:4000 \
+  -e JEKYLL_ACTION \
+  -e JEKYLL_CONFIG \
+  -e JEKYLL_ENV \
+  -v /etc/passwd:/etc/passwd:ro \
+  -v /etc/group:/etc/group:ro \
+  -u "$(id -u)":"$(id -g)" \
+  -v "$(pwd)":/srv/source \
+  linaroits/jekyllsitebuild:"$JEKYLLSITEBUILD" \
+  build-site.sh

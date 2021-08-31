c00d8b3f2ea8270915146030aa6e6162c9fdfcb9
Daniel Díaz <daniel.diaz@linaro.org>
2021-08-30 21:17:10 -0500

    getsla: Output number of regressions too
    This shows the total number of regressions reported in the
    given RC Log.
    
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- _pages/getsla.py
+++ _pages/getsla.py
@@ -7,18 +7,25 @@ SLA = {
     '<24': 0,
     '<8': 0,
 }
+regressions = 0
 
 filename = 'index.md'
 if len(sys.argv) > 1:
     filename = sys.argv[1]
+
 with open(filename) as f:
     for line in f.readlines():
-        if ' sla ' in line:
+        if '<!-- sla ' in line:
             (_, _, hour, count, _) = line.split(' ')
             SLA[str(hour)] += int(count)
+        if '<!-- regressions ' in line:
+            (_, _, count, _) = line.split(' ')
+            regressions += int(count)
+
 total = sum(SLA.values())
 print("Total RCs: {}".format(total))
 for sla, count in SLA.items():
     print("  SLA {}: {}".format(sla, count))
 total_met = SLA['<48'] + SLA['<24'] + SLA['<8']
 print("SLA met {:.1f}% of the time".format(100*total_met/total))
+print("Regressions reported: {:d}".format(regressions))
